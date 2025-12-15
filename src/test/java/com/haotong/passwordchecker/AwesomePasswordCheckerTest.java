664114806f37919d2ae36b610326dc46927c1de3
TLamarc <chen_ellamar@163.com>
2025-12-15 08:54:57 +0100

    ADD : Junit

--- src/test/java/com/haotong/passwordchecker/AwesomePasswordCheckerTest.java
+++ src/test/java/com/haotong/passwordchecker/AwesomePasswordCheckerTest.java
@@ -8,7 +8,7 @@ class AwesomePasswordCheckerTest {
     @Test
     void md5ShouldMatchKnownValue() {
         // MD5("abc") = 900150983cd24fb0d6963f7d28e17f72
-        String hash = AwesomePasswordChecker.computeMd5("abc");
+        String hash = AwesomePasswordChecker.ComputeMD5("abc");
         assertEquals("900150983cd24fb0d6963f7d28e17f72", hash);
     }
 }
